41cf0896141ae4ac49c40c2d30c23c2a1332f3d0
Mariana Martinez <marimar.lunam@gmail.com>
2024-06-06 18:12:02 -0500

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 // Conectar a MongoDB
 mongoose.connect('mongodb://localhost:27017/testapp');
 
